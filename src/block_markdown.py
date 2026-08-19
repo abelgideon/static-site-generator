a42dc7cbe76a8bef7e561eef594e52c7d9697f33
Abel Gideon <abelgideontk7@gmail.com>
2026-08-19 15:18:22 +0300

    feat: create block to block type converter

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,15 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    ULIST = "unordered_list"
+    OLIST = "ordered_list"
+
+
 def markdown_to_blocks(markdown: str):
     blocks = markdown.split("\n\n")
     filtered: list[str] = []
@@ -9,3 +21,31 @@ def markdown_to_blocks(markdown: str):
         filtered.append(block)
 
     return filtered
+
+
+def block_to_block_type(block: str) -> BlockType:
+    lines = block.splitlines()
+
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    elif len(lines) > 1 and block.startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
+    elif block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    elif block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.ULIST
+    elif block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.OLIST
+
+    return BlockType.PARAGRAPH
